7809ba7087e8f856a54635c91ecddad3a9343855
neeptoon <neeptoon@mail.ru>
2020-08-19 12:26:29 +0300

    Реализует показ сообщения об отсутствии фильмов в системе

--- src/view/films-list.js
+++ src/view/films-list.js
@@ -2,7 +2,7 @@ import {createElement} from "../utils.js";
 
 const createFilmsListHTML = () =>
   `<section class="films-list">
-      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      
     </section >`;
 
 export default class FilmsList {
